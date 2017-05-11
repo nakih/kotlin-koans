95d6764551514d0440d944aa5249d39d3c1607f6
Michael Weilbächer <git@mweilb.de>
2017-05-11 15:06:58 +0200

    task25

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,19 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        if (this.year != other.year) {
+            return this.year.compareTo(other.year)
+        }
+        if (this.month != other.month) {
+            return this.month.compareTo(other.month)
+        }
+        if (this.dayOfMonth != other.dayOfMonth) {
+            return this.dayOfMonth.compareTo(other.dayOfMonth)
+        }
+        return 0
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
